c5f07c65907d185ec76bd911241ac6b10145fcc0
mikhail <3943804+Thecentury@users.noreply.github.com>
2010-11-20 20:45:06 +0300

    Updated DataRect tests

--- Main/src/DynamicDataDisplay.Tests/D3/DataRectTest.cs
+++ Main/src/DynamicDataDisplay.Tests/D3/DataRectTest.cs
@@ -20,5 +20,42 @@ namespace DynamicDataDisplay.Tests.D3
 			Assert.IsTrue(infinite.Contains(new DataRect(-100, -100, 1, 1)));
 			Assert.IsTrue(infinite.Contains(new Point(0, 0)));
 		}
+
+		[TestMethod]
+		public void Contains_1()
+		{
+			var big = DataRect.Create(0, 0, 1, 1);
+			var small = DataRect.Create(0.1, 0.1, 0.9, 0.9);
+
+			Assert.IsTrue(big.Contains(small));
+			Assert.IsTrue(big.IntersectsWith(small));
+			Assert.IsTrue(small.IntersectsWith(big));
+			Assert.IsFalse(small.Contains(big));
+		}
+
+		[TestMethod]
+		public void Intersects_1()
+		{
+			var first = DataRect.Create(0, 0, 1, 1);
+			var second = DataRect.Create(-0.5, -0.5, 0.5, 0.5);
+
+			Assert.IsTrue(first.IntersectsWith(second));
+			Assert.IsTrue(second.IntersectsWith(first));
+
+			Assert.IsFalse(first.Contains(second));
+			Assert.IsFalse(second.Contains(first));
+
+			var intersection1 = first;
+			intersection1.Intersect(second);
+			var intersection2 = second;
+			intersection2.Intersect(first);
+
+			var expected = DataRect.Create(0, 0, 0.5, 0.5);
+
+			Assert.AreEqual(expected, intersection1);
+			Assert.AreEqual(expected, intersection2);
+		}
+
+
 	}
 }

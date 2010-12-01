0c66cfdb83927a6eff2e1bf1c874ada0590af54b
mikhail <3943804+Thecentury@users.noreply.github.com>
2010-12-01 07:56:12 +0300

    PivotSegmentEditor is more ready that it was in previous commit.

--- Main/src/DynamicDataDisplay/Charts/Shapes/PivotSegmentEditor.cs
+++ Main/src/DynamicDataDisplay/Charts/Shapes/PivotSegmentEditor.cs
@@ -13,6 +13,7 @@ using System.Windows.Controls;
 using System.Diagnostics.Contracts;
 using System.Windows.Shapes;
 using System.Windows.Media;
+using System.Diagnostics;
 
 namespace Microsoft.Research.DynamicDataDisplay.Charts.Shapes
 {
@@ -26,6 +27,8 @@ namespace Microsoft.Research.DynamicDataDisplay.Charts.Shapes
 		private Func<double, string> xMapping = d => d.ToString("F");
 		private readonly ViewportRay leftRay = new ViewportRay { StrokeDashArray = new DoubleCollection(new double[] { 1, 1 }), Direction = -1 };
 		private readonly ViewportRay rightRay = new ViewportRay { StrokeDashArray = new DoubleCollection(new double[] { 1, 1 }), Direction = +1 };
+		private readonly Border leftBorder = new Border();
+		private readonly Border rightBorder = new Border();
 
 		/// <summary>
 		/// Initializes a new instance of the <see cref="PivotSegmentEditor"/> class.
@@ -92,7 +95,7 @@ namespace Microsoft.Research.DynamicDataDisplay.Charts.Shapes
 
 			GenericValueConverter<Point> pointTextConverter = new GenericValueConverter<Point>(p =>
 			{
-				var result = p; //String.Format("({0}, {1})", xMapping(p.X), p.Y);
+				var result = String.Format("({0}, {1})", xMapping(p.X), p.Y);
 				return result;
 			});
 			Grid leftPointGrid = new Grid();
@@ -102,7 +105,6 @@ namespace Microsoft.Research.DynamicDataDisplay.Charts.Shapes
 			leftPointGrid.Children.Add(leftPointText);
 			panel.Children.Add(leftPointGrid);
 			leftPointText.SetBinding(TextBlock.TextProperty, new Binding("Point1") { Source = this, Converter = pointTextConverter });
-			leftPointText.HorizontalAlignment = HorizontalAlignment.Stretch;
 
 			leftPointGrid.SetBinding(ViewportPanel.XProperty, new Binding("Point1.X") { Source = this });
 			leftPointGrid.SetBinding(ViewportPanel.YProperty, new Binding("Point1.Y") { Source = this });
@@ -121,6 +123,33 @@ namespace Microsoft.Research.DynamicDataDisplay.Charts.Shapes
 			rightPointGrid.SetBinding(ViewportPanel.YProperty, new Binding("Point2.Y") { Source = this });
 			ViewportPanel.SetViewportHorizontalAlignment(rightPointGrid, HorizontalAlignment.Left);
 			ViewportPanel.SetViewportVerticalAlignment(rightPointGrid, VerticalAlignment.Top);
+
+			TextBlock leftText = new TextBlock { Margin = new Thickness(3) };
+			ViewportPanel.SetViewportHorizontalAlignment(leftBorder, HorizontalAlignment.Left);
+			ViewportPanel.SetViewportVerticalAlignment(leftBorder, VerticalAlignment.Top);
+			Binding leftYBinding = new Binding("LeftY") { Source = this };
+			leftText.SetBinding(TextBlock.TextProperty, leftYBinding);
+			leftBorder.SetBinding(ViewportPanel.YProperty, leftYBinding);
+			leftBorder.Child = leftText;
+			leftBorder.SetBinding(ViewportPanel.ViewportVerticalAlignmentProperty, centerPointVerticalAlignmentBinding);
+
+			TextBlock rightText = new TextBlock { Margin = new Thickness(3) };
+			ViewportPanel.SetViewportHorizontalAlignment(rightBorder, HorizontalAlignment.Right);
+			ViewportPanel.SetViewportVerticalAlignment(rightBorder, VerticalAlignment.Top);
+			Binding rightYBinding = new Binding("RightY") { Source = this };
+			rightText.SetBinding(TextBlock.TextProperty, rightYBinding);
+			rightBorder.SetBinding(ViewportPanel.YProperty, rightYBinding);
+			rightBorder.Child = rightText;
+			rightBorder.SetBinding(ViewportPanel.ViewportVerticalAlignmentProperty, centerPointVerticalAlignmentBinding);
+
+			leftBorder.SetBinding(Border.BorderBrushProperty, strokeBinding);
+			leftBorder.SetBinding(Border.BorderThicknessProperty, strokeThicknessBinding);
+
+			rightBorder.SetBinding(Border.BorderBrushProperty, strokeBinding);
+			rightBorder.SetBinding(Border.BorderThicknessProperty, strokeThicknessBinding);
+
+			panel.Children.Add(leftBorder);
+			panel.Children.Add(rightBorder);
 		}
 
 		#region Properties
@@ -157,6 +186,8 @@ namespace Microsoft.Research.DynamicDataDisplay.Charts.Shapes
 		private static void OnPointChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
 		{
 			PivotSegmentEditor owner = (PivotSegmentEditor)d;
+			owner.CoerceValue(LeftYProperty);
+			owner.CoerceValue(RightYProperty);
 			owner.OnPointChanged();
 		}
 
@@ -212,6 +243,78 @@ namespace Microsoft.Research.DynamicDataDisplay.Charts.Shapes
 
 		#endregion
 
+		#region LeftY property
+
+		public double LeftY
+		{
+			get { return (double)GetValue(LeftYProperty); }
+		}
+
+		private static readonly DependencyPropertyKey LeftYPropertyKey = DependencyProperty.RegisterReadOnly(
+			"LeftY",
+			typeof(double),
+			typeof(PivotSegmentEditor),
+			new FrameworkPropertyMetadata(null, OnLeftYCoerce));
+
+		public static readonly DependencyProperty LeftYProperty = LeftYPropertyKey.DependencyProperty;
+
+		private Func<double, double> GetLineFunc()
+		{
+			Viewport2D viewport = plotter.Viewport;
+			double deltaX = Point1.X - Point2.X;
+			double deltaY = Point1.Y - Point2.Y;
+			double m = deltaY / deltaX;
+			double b = Point1.Y - Point1.X * deltaY / deltaX;
+
+			Func<double, double> func = x => m * x + b;
+
+			return func;
+		}
+
+		private static object OnLeftYCoerce(DependencyObject source, object value)
+		{
+			PivotSegmentEditor editor = (PivotSegmentEditor)source;
+			if (editor.plotter == null)
+				return value;
+
+			Func<double, double> func = editor.GetLineFunc();
+			double xmin = editor.plotter.Viewport.Visible.XMin;
+			double result = func(xmin);
+
+			return result;
+		}
+
+		#endregion
+
+		#region RightY property
+
+		public double RightY
+		{
+			get { return (double)GetValue(RightYProperty); }
+		}
+
+		private static readonly DependencyPropertyKey RightYPropertyKey = DependencyProperty.RegisterReadOnly(
+			"RightY",
+			typeof(double),
+			typeof(PivotSegmentEditor),
+			new FrameworkPropertyMetadata(null, OnRightYCoerce));
+
+		public static readonly DependencyProperty RightYProperty = RightYPropertyKey.DependencyProperty;
+
+		private static object OnRightYCoerce(DependencyObject source, object value)
+		{
+			PivotSegmentEditor editor = (PivotSegmentEditor)source;
+			if (editor.plotter == null)
+				return value;
+
+			Func<double, double> func = editor.GetLineFunc();
+			double xmax = editor.plotter.Viewport.Visible.XMax;
+			double result = func(xmax);
+
+			return result;
+		}
+		#endregion
+
 		#endregion
 
 		#region IPlotterElement Members
@@ -219,6 +322,8 @@ namespace Microsoft.Research.DynamicDataDisplay.Charts.Shapes
 		public void OnPlotterAttached(Plotter plotter)
 		{
 			this.plotter = (Plotter2D)plotter;
+
+			this.plotter.Viewport.PropertyChanged += OnViewport_PropertyChanged;
 
 			plotter.Dispatcher.BeginInvoke(() =>
 			{
@@ -229,11 +334,25 @@ namespace Microsoft.Research.DynamicDataDisplay.Charts.Shapes
 					panel,
 					leftRay,
 					rightRay);
+
+				leftBorder.SetBinding(ViewportPanel.XProperty, new Binding("Visible.XMin") { Source = this.plotter.Viewport });
+				rightBorder.SetBinding(ViewportPanel.XProperty, new Binding("Visible.XMax") { Source = this.plotter.Viewport });
+				CoerceValue(LeftYProperty);
+				CoerceValue(RightYProperty);
+
 			}, DispatcherPriority.Normal);
+		}
+
+		private void OnViewport_PropertyChanged(object sender, ExtendedPropertyChangedEventArgs e)
+		{
+			CoerceValue(LeftYProperty);
+			CoerceValue(RightYProperty);
 		}
 
 		public void OnPlotterDetaching(Plotter plotter)
 		{
+			this.plotter.Viewport.PropertyChanged -= OnViewport_PropertyChanged;
+
 			plotter.Dispatcher.BeginInvoke(() =>
 			{
 				plotter.Children.RemoveAll(

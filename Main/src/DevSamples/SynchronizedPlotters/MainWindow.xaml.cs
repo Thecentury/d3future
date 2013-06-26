48c9fa8f8dffeba4bf40c060bb965c81195e2177
mikhail.brinchuk <3943804+Thecentury@users.noreply.github.com>
2013-06-26 21:54:46 +0400

    Added sample showing two plotters with synchronized viewports.

---
+++ Main/src/DevSamples/SynchronizedPlotters/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Research.DynamicDataDisplay;
+
+namespace SynchronizedPlotters
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            plotter1.Viewport.SetBinding( Viewport2D.VisibleProperty, new Binding( "Visible" ) { Source = plotter2.Viewport, Mode = BindingMode.TwoWay} );
+        }
+    }
+}
